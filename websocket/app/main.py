5f9a660f6eb3f5c866401daa4b2a6df370a6b37d
HarshiniSaminathan <saminathan.harshini@divum.in>
2024-10-07 12:08:40 +0530

    Web socket connection connected & disconnected logic added

---
+++ websocket/app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*", transports=['websocket', 'polling'])
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message received: ' + msg)
+    send(msg, broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@app.route('/')
+def index():
+    return jsonify({"message":"Welcome to Flask WebSocket Example!"})
+
+ # pip freeze > requirements.txt
+# pip install - r requirements.txt
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
